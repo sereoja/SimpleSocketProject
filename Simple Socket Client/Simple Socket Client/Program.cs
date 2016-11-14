66de589cd04accb8cad2ec76b8bd37c4ab5906cb
Sergiu <sereoja@hotmail.com>
2016-11-14 13:15:58 -0800

    Simple Socket first commit

---
+++ Simple Socket Client/Simple Socket Client/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//add
+using System.Net;
+using System.Net.Sockets;
+
+namespace Simple_Socket_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter server ip address");
+            //string strIPaddress = Console.ReadLine();
+            //Console.Write("Enter port #: ");
+            //int port = int.Parse(Console.ReadLine());
+
+            Socket client = null;
+            try
+            {
+
+           
+            IPAddress ipaddress = IPAddress.Parse("172.20.2.247");
+            int port = 9000;
+
+            //create an endpoint that represesnts the server endpoint
+            //1. Create an IP EndPoint
+            IPEndPoint serverEndP = new IPEndPoint(ipaddress, port);
+            //2. Create a socket
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+                ProtocolType.Tcp);
+            //connect to the server
+            client.Connect(serverEndP);
+            //causes the listen method of the sercer to add the client
+            //to the queue and causes the Accept method of the server
+            //to get this client
+
+            //now have the client send a request
+            //use the send method
+
+
+            Console.WriteLine("\nEnter Request: ");
+            string request = Console.ReadLine();
+            //convert the request to a byte array
+            byte[] msg = Encoding.ASCII.GetBytes(request);
+            //send it
+            client.Send(msg);
+
+            //then have the client wait for a response from server
+            //use the receive method to get a response from server
+            byte[] buffer = new byte[1024];
+            int bytesReceived = client.Receive(buffer);
+
+            //convert byte array to a string
+            string response = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+
+            //display
+            Console.WriteLine("\nServer Response: \n");
+            Console.WriteLine(response);
+           // 
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+            finally
+            {
+                //close the socket
+                if (client!=null)
+                {
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Simple Socket Server/Simple Socket Server/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//add
+using System.Net;
+using System.Net.Sockets;
+
+namespace Simple_Socket_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get ip address of this local computer
+            //get the name of this (local) computer
+            try
+            {
+                const int port = 9000;
+
+                string localHost = Dns.GetHostName();
+
+                // get IP address of this computer
+                IPHostEntry hostEntry = Dns.GetHostEntry(localHost);
+                //the hostEntry defines a property AddressList, w
+                //which is an array of type IPAddress
+
+                IPAddress[] ipaddresses = hostEntry.AddressList;
+                //display the ip address associated with this computer
+
+
+                //1. Create an IP EndPoint
+                IPEndPoint endP = new IPEndPoint(ipaddresses[1], port);
+                //2. Create a socket
+                Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+                    ProtocolType.Tcp);
+                //3. bind this endpoint to the socket
+                server.Bind(endP);
+
+                //4. set the socket to listen for incoming request,
+                //   then queue them
+                server.Listen(10); //10 means the size of the queue
+                //Listen start internal thread that is a loop that listen and puts requeste in a queue
+                //5. Accept the requests
+                while (true)
+                {
+                    Console.WriteLine("Waiting for client to connect at {0}:{1}...", ipaddresses[1], port);
+                    //get the newxt client (if any) from the queue
+                    Socket client = server.Accept();
+
+                    //find out about the client and save or display the client info
+                    DisplayClientInfo(client);
+
+          //receive request client
+                    byte[] buffer = new byte[1024];
+                    int bytesReceived = client.Receive(buffer);
+
+                    //convert byte array to a string
+                    string request = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+                    Console.WriteLine("\nClient request: ");
+                    Console.WriteLine(request);
+                    //process the request
+                    string response = ProcessClient(client, request);
+
+                    Console.WriteLine("Respond: ");
+                    //string response = Console.ReadLine();
+
+
+                    //string info = "\nrequest received: " + DateTime.Now;
+                    //send a response
+                    //byte[] msg = Encoding.ASCII.GetBytes(response + info);
+                    byte[] msg = Encoding.ASCII.GetBytes(response);
+                    client.Send(msg);
+                }
+               // 
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+            Console.ReadLine();
+        } //end of main
+        static void DisplayClientInfo(Socket client)
+        {
+            //get the client endpoin using the RemoteEndPoint property
+            IPEndPoint clientEndP = (IPEndPoint)client.RemoteEndPoint;
+            //now we can extract its IP Address and port number
+            IPAddress clientIPAddress = clientEndP.Address;
+            //client port
+            int clientPort = clientEndP.Port;
+            //get tje client domain name
+            string clientName = Dns.GetHostEntry(clientIPAddress).HostName;
+            //save to a file or display
+            Console.WriteLine("\nClient Request from {0} @ {1}:{2}", clientName, clientIPAddress, clientPort);
+        }
+
+        static string ProcessClient(Socket client, string request)
+        {
+            if (request == string.Empty)
+            {
+                return "Error: Request not in proper format";
+            }
+
+            //split
+            string[] tokens = request.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            if (tokens.Length < 3)
+                return "Error: request not in proper format";
+            string response = string.Empty;
+
+            switch (tokens[0].ToLower())
+            {
+                case "add":
+                    {
+                        double x, y;
+                        double.TryParse(tokens[1], out x);
+                        double.TryParse(tokens[2], out y);
+                        double result = x + y;
+                        response = result.ToString();
+                    }
+                    break;
+                default:
+                    response = "invalid operation";
+                    break;
+            } //end of switch
+            return response;
+        }
+    }
+}
